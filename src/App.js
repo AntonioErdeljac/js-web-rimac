1da5ef02c06f8262746c32a6824e3703211c212c
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-02 02:10:30 +0100

    feat(cpu): connect firebase to show realtime cpu temperature using js-cpu-rimac module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+})
+
+const db = firebase.database();
 
 const App = () => {
+  const [temp, setTemp] = useState(0)
+  useEffect(() => {
+    db.ref('cpu').on('value', snapshot => {
+      setTemp(snapshot.val()?.temperature)
+    });
+
+  }, [])
+
   return (
-    <div>Hello Rimac</div>
+  <div>CPU Temp: {temp}</div>
   );
 }
 
