1a45b24c51be98f416b2a642ee5772edac78d971
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-02 02:18:28 +0100

    refactor(firebase): move firebase to separate util

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import firebase from 'firebase';
-import 'firebase/firestore';
 
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-})
+import { firebase } from './utils'
 
-const db = firebase.database();
 
 const App = () => {
   const [temp, setTemp] = useState(0)
   useEffect(() => {
-    db.ref('cpu').on('value', snapshot => {
+    firebase.db.ref('cpu').on('value', snapshot => {
       setTemp(snapshot.val()?.temperature)
     });
 
