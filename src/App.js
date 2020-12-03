23adddc5be2cb1c90071df910e9f95e889f8a8bd
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-03 01:22:08 +0100

    feat(lint): add eslint, prettier, commitlint, fix lint errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { isFuture } from 'date-fns'
+import React, { useEffect, useState } from 'react';
+import { isFuture } from 'date-fns';
 
-import { firebase } from './utils'
+import { firebase } from './utils';
 
 const CONNECTION_MS = 1000;
 
 const App = () => {
-  const [info, setInfo] = useState({})
-  const [online, setOnline] = useState(false)
+  const [info, setInfo] = useState({});
+  const [online, setOnline] = useState(false);
 
   useEffect(() => {
-    firebase.db.ref('info').on('value', snapshot => {
-      setInfo(snapshot.val() || {})
+    firebase.db.ref('info').on('value', (snapshot) => {
+      setInfo(snapshot.val() || {});
     });
-  }, [])
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setOnline(isFuture(new Date(info.expiresAt)))
-    }, CONNECTION_MS)
+      setOnline(isFuture(new Date(info.expiresAt)));
+    }, CONNECTION_MS);
 
     return () => clearInterval(interval);
-  }, [info.expiresAt])
+  }, [info.expiresAt]);
 
   return (
     <>
@@ -31,6 +31,6 @@ const App = () => {
       <div>Online: {`${online}`}</div>
     </>
   );
-}
+};
 
 export default App;
