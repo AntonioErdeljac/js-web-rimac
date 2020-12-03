2a951444e720cb72cdeedafcf5fe5d6ff4731bec
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-03 01:09:19 +0100

    feat(online): add display and observance of online status

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react'
+import { isFuture } from 'date-fns'
 
 import { firebase } from './utils'
 
+const CONNECTION_MS = 1000;
 
 const App = () => {
-  const [temp, setTemp] = useState(0)
-  useEffect(() => {
-    firebase.db.ref('cpu').on('value', snapshot => {
-      setTemp(snapshot.val()?.temperature)
-    });
+  const [info, setInfo] = useState({})
+  const [online, setOnline] = useState(false)
 
+  useEffect(() => {
+    firebase.db.ref('info').on('value', snapshot => {
+      setInfo(snapshot.val() || {})
+    });
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOnline(isFuture(new Date(info.expiresAt)))
+    }, CONNECTION_MS)
+
+    return () => clearInterval(interval);
+  }, [info.expiresAt])
+
   return (
-  <div>CPU Temp: {temp}</div>
+    <>
+      <div>CPU Temp: {info.temperature} C°</div>
+      <div>Battery percentage: {info.batteryPercentage}%</div>
+      <div>Used memory percentage: {info.memoryPercentage}%</div>
+      <div>Online: {`${online}`}</div>
+    </>
   );
 }
 
