e085dc0cc9e9d2e4562b7307f53110fa61c53826
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-04 15:43:25 +0100

    feat(tests): add drawer component unit test

---
+++ src/components/Drawer/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+
+import Drawer from './index';
+
+const makeProps = ({ children } = {}) => ({
+  children: children || <p>Content</p>,
+});
+
+const renderComponent = (props) => shallow(<Drawer {...props} />);
+
+describe('Drawer Component', () => {
+  it('renders correctly according to snapshot', () => {
+    expect(toJson(renderComponent(makeProps()))).toMatchSnapshot();
+  });
+
+  it('renders proper children', () => {
+    const expectedChildren = 'Lorem';
+
+    const wrapper = renderComponent(makeProps({ children: expectedChildren }));
+    expect(wrapper.children().debug()).toEqual(expectedChildren);
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,15 @@
-import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
+
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {},
+    };
+  },
+});
+
+Enzyme.configure({ adapter: new Adapter() });
