ce35e0f15147a5641dd4789017dd769812b39d97
Antonio Erdeljac <hedhogs@gmail.com>
2020-12-04 02:16:57 +0100

    feat(settings): implement settings page

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -2,14 +2,14 @@ import CountUp from 'react-countup';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React, { memo, useEffect, useMemo } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, FormControl } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './index.scss';
 
 import { useSystem } from '../../providers';
 
-const Info = ({ icon, title, value, suffix, min, max, warning }) => {
+const Info = ({ icon, title, value, suffix, min, max, warning, edit, onChange }) => {
   const { warn, clearWarning } = useSystem();
 
   const isFaulty = useMemo(() => {
@@ -20,12 +20,12 @@ const Info = ({ icon, title, value, suffix, min, max, warning }) => {
   }, [value, min, max]);
 
   useEffect(() => {
-    if (isFaulty) {
+    if (isFaulty && !edit) {
       warn(warning);
-    } else {
+    } else if (!edit) {
       clearWarning(warning);
     }
-  }, [isFaulty, warning, warn, clearWarning]);
+  }, [isFaulty, warning, warn, clearWarning, edit]);
 
   return (
     <Card className="r-info mb-4">
@@ -34,17 +34,26 @@ const Info = ({ icon, title, value, suffix, min, max, warning }) => {
         <div className="mt-4">
           <strong className="text-primary">{title}</strong>
         </div>
-        <div className="mt-4">
-          <strong className={cn({ 'text-danger': isFaulty })}>
-            <CountUp preserveValue duration={5} end={value} />
-            {suffix}
-          </strong>
-        </div>
-        <hr className="w-100" />
-        <div className="d-inline-flex align-items-center justify-content-between w-100 px-5">
-          <small className="text-muted">Max: {max}</small>
-          <small className="text-muted">Min: {min}</small>
-        </div>
+        {edit ? (
+          <div className="mt-3">
+            <FormControl className="my-2" placeholder="Min" onChange={onChange} name="min" />
+            <FormControl placeholder="Max" onChange={onChange} name="max" />
+          </div>
+        ) : (
+          <>
+            <div className="mt-4">
+              <strong className={cn({ 'text-danger': isFaulty })}>
+                <CountUp preserveValue duration={5} end={value} />
+                {suffix}
+              </strong>
+            </div>
+            <hr className="w-100" />
+            <div className="d-inline-flex align-items-center justify-content-between w-100 px-5">
+              <small className="text-muted">Max: {max}</small>
+              <small className="text-muted">Min: {min}</small>
+            </div>
+          </>
+        )}
       </Card.Body>
     </Card>
   );
@@ -57,7 +66,9 @@ Info.propTypes = {
   suffix: PropTypes.string,
   min: PropTypes.number,
   max: PropTypes.number,
-  warning: PropTypes.string.isRequired,
+  warning: PropTypes.string,
+  edit: PropTypes.bool,
+  onChange: PropTypes.func,
 };
 
 Info.defaultProps = {
@@ -65,6 +76,9 @@ Info.defaultProps = {
   value: 0,
   min: -Infinity,
   max: Infinity,
+  edit: false,
+  onChange: () => {},
+  warning: '',
 };
 
 export default memo(Info);
--- src/views/Settings/index.jsx
+++ src/views/Settings/index.jsx
@@ -1,10 +1,28 @@
 import React, { memo } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Fade } from 'react-reveal';
+import { Info } from '../../components';
 
 const Settings = () => {
   return (
-    <div>
-      <p>Settings</p>
-    </div>
+    <Fade left>
+      <div className="r-settings mt-5">
+        <Row>
+          <Col lg={3} md={6}>
+            <Info title="Battery" icon="bolt" edit />
+          </Col>
+          <Col lg={3} md={6}>
+            <Info title="Network Connections" icon="network-wired" edit />
+          </Col>
+          <Col lg={3} md={6}>
+            <Info title="CPU Temperature" icon="thermometer-three-quarters" edit />
+          </Col>
+          <Col lg={3} md={6}>
+            <Info title="Memory Usage" icon="memory" edit />
+          </Col>
+        </Row>
+      </div>
+    </Fade>
   );
 };
 
